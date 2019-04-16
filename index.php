9b7ca4966ac0a73dd82665f60778a4d52b5ce9d4
Sasha Renninger <sashafr@upenn.edu>
2019-04-16 21:41:55 +0000

    added recent and featured sections to home page

--- index.php
+++ index.php
@@ -51,4 +51,53 @@
     </div>
 </div>
 
+<?php if (get_theme_option('Display Featured Item') == '1' || get_theme_option('Display Featured Exhibit') == '1' || get_theme_option('Display Recent Item') == '1'): ?>
+    <div class="fh5co-cards">
+        <div class="fh5co-narrow-content">
+            <h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Browse</h2>
+
+            <div class="fh5co-flex-wrap">
+                <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
+                    <?php $feat_item = get_random_featured_items(1); ?>
+                    <?php if (count($feat_item) > 0): ?>
+                        <div class="fh5co-card animate-box" data-animate-effect="fadeInLeft">
+                            <div class="row video-row"><a href="<?php echo record_url($feat_item[0]) ?>"><h5>Featured Item: <?php echo metadata($feat_item[0],array('Dublin Core','Title')) ?></h5></a></div>
+                            <div class="row video-row browse-box-row">
+                                <div class="ps-browse-box-image"><?php echo display_video($feat_item[0]) ?></div>
+                                <p><?php echo metadata($feat_item[0],array('Dublin Core','Description')) ?></p>
+                            </div>
+                            <div class="row video-row"><p><a href="<?php echo record_url($feat_item[0]) ?>" class="btn btn-md btn-primary">Learn More</a></p></div>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+                <?php if (get_theme_option('Display Recent Item') !== '0'): ?>
+                    <?php $recent_item = get_recent_items(1); ?>
+                    <?php if (count($recent_item) > 0): ?>
+                        <div class="fh5co-card animate-box" data-animate-effect="fadeInLeft">
+                            <div class="row video-row"><a href="<?php echo record_url($recent_item[0]) ?>"><h5>Recently Added: <?php echo metadata($recent_item[0],array('Dublin Core','Title')) ?></h5></a></div>
+                            <div class="row video-row browse-box-row">
+                                <div class="ps-browse-box-image"><?php echo display_video($recent_item[0]) ?></div>
+                                <p><?php echo metadata($recent_item[0],array('Dublin Core','Description')) ?></p>
+                            </div>
+                            <div class="row video-row"><p><a href="<?php echo record_url($recent_item[0]) ?>" class="btn btn-md btn-primary">Learn More</a></p></div>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+                <?php if (get_theme_option('Display Featured Exhibit') !== '0' && plugin_is_active('ExhibitBuilder') && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+                    <?php $feat_ex = exhibit_builder_random_featured_exhibit(); ?>
+                    <?php if ($feat_ex): ?>
+                        <div class="fh5co-card animate-box" data-animate-effect="fadeInLeft">
+                            <div class="row video-row"><h5>Featured Exhibit: <?php echo exhibit_builder_link_to_exhibit($feat_ex) ?></h5></div>
+                            <div class="row video-row browse-box-row">
+                                <p><?php echo strip_formatting(metadata($feat_ex, 'description', array('snippet' => 200))); ?></p>
+                            </div>
+                            <div class="row video-row"><p><?php echo exhibit_builder_link_to_exhibit($feat_ex, "Learn More", array('class' => 'btn btn-md btn-primary'), null); ?></p></div>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+
+        </div>
+    </div>
+<?php endif; ?>
 <?php echo foot(); ?>
