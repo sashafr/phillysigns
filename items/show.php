b9496f25413317c86e277d39407049cbb6740bd8
Sasha Renninger <sashafr@upenn.edu>
2019-05-16 07:16:03 +0000

    cleaned up items show

--- items/show.php
+++ items/show.php
@@ -12,24 +12,59 @@
 
 			<div class="col-md-12">
 				<h1><?php echo metadata($item,array('Dublin Core','Title')) ?></h1>
-                <?php if (count($item->getItemTypeElements()) > 0): ?>
-                    <table>
-                        <?php foreach (all_element_texts($item, array('return_type' => 'array')) as $elementset => $elements): ?>
-                            <?php foreach ($elements as $element => $elementtexts): ?>
-                                <?php if ($element != "Title" && $element != "Interviewee" && $element != "Embedded Video"): ?>
-                                    <tr class="element" >
-                                        <td style="vertical-align: top; width: 40%; font-weight: bold;"><?php echo $element ?>:</td>
-                                        <td style="vertical-align: top;">
-                                            <?php foreach ($elementtexts as $elementtext): ?>
-                                                <?php echo $elementtext ?></br>
-                                            <?php endforeach ?>
-                                        </td>
-                                    </tr>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
+                <table>
+                    <?php foreach (all_element_texts($item, array('return_type' => 'array')) as $elementset => $elements): ?>
+                        <?php foreach ($elements as $element => $elementtexts): ?>
+                            <?php if ($element != "Title" && $element != "Interviewee" && $element != "Embedded Video"): ?>
+                                <tr class="element" >
+                                    <td style="vertical-align: top; width: 40%; font-weight: bold;"><?php echo $element ?>:</td>
+                                    <td style="vertical-align: top;">
+                                        <?php foreach ($elementtexts as $elementtext): ?>
+                                            <?php echo $elementtext ?></br>
+                                        <?php endforeach ?>
+                                    </td>
+                                </tr>
+                            <?php endif; ?>
                         <?php endforeach; ?>
-                    </table>
-                <?php endif ?>
+                    <?php endforeach; ?>
+                </table>
+
+                <!-- The following returns all of the files associated with an item. -->
+                <?php if (metadata('item', 'has files')): ?>
+                    <div id="itemfiles" class="element">
+                        <h3><?php echo __('Files'); ?></h3>
+                        <?php echo files_for_item(); ?>
+                    </div>
+                <?php endif; ?>
+
+                <!-- If the item belongs to a collection, the following creates a link to that collection. -->
+                <?php if (metadata('item', 'Collection Name')): ?>
+                    <div id="collection" class="element item-collection">
+                        <h3><?php echo __('Collection'); ?></h3>
+                        <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+                    </div>
+                <?php endif; ?>
+
+                <!-- The following prints a list of all tags associated with the item -->
+                <?php if (metadata('item', 'has tags')): ?>
+                <div id="item-tags" class="element">
+                    <h3><?php echo __('Tags'); ?></h3>
+                    <div class="element-text"><?php echo tag_string('item'); ?></div>
+                </div>
+                <?php endif;?>
+
+                <!-- The following prints a citation for this item. -->
+                <div id="item-citation" class="element">
+                    <h3><?php echo __('Citation'); ?></h3>
+                    <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+                </div>
+
+                <div id="item-output-formats" class="element">
+                    <h3><?php echo __('Output Formats'); ?></h3>
+                    <div class="element-text"><?php echo output_format_list(); ?></div>
+                </div>
+
+                <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>                
 			</div>
 
 		</div>
